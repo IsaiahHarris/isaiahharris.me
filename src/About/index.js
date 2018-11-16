9534063586759f3f01bf7a6d0fc1a26a98dfcfac
Isaiah Harris <harrisfambam@gmail.com>
2018-11-16 12:23:26 -1000

    change font spacing

--- src/About/index.js
+++ src/About/index.js
@@ -9,20 +9,20 @@ class About extends Component {
           <div className="sub-title">Hi, I'm Isaiah Harris</div>
         </div>
         <div className="body">
-          <p>
+          <div>
             After discovering coding in High-School, I've been determined to
             improve myself as a developer everyday.
-          </p>
-          <p>
+          </div>
+          <div>
             Four months after I graduated, I completed an immersive Javascript
             Coding Bootcamp in Honolulu, Hawaii. Since then I have enjoyed
             participating in multiple Hackathons and Meetups. I also have taken
             multiple online courses to enhance my skills.
-          </p>
-          <p>
+          </div>
+          <div>
             I'm passionate, always ready for a challenge, and open to new
             experiences.
-          </p>
+          </div>
         </div>
         <div className="footer">logos</div>
       </div>
