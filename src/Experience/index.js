bb6818a80f5ba35ea52cdd83cf27da251e815c79
Isaiah Harris <harrisfambam@gmail.com>
2018-12-05 12:28:04 -1000

    switch page between work and projects

--- src/Experience/index.js
+++ src/Experience/index.js
@@ -2,10 +2,45 @@ import React from 'react';
 import './Experience.scss';
 
 const experience = () => {
-  console.log();
+  function showProjects(subject) {
+    let projectsContainer = document.getElementById('projects-container');
+    let workContainer = document.getElementById('work-container');
+    if (subject === 'projects') {
+      projectsContainer.style.zIndex = 1;
+      workContainer.style.zIndex = 0;
+    } else if (subject === 'work') {
+      projectsContainer.style.zIndex = 0;
+      workContainer.style.zIndex = 1;
+    }
+  }
+
   return (
     <div className="experience-container">
       <div className="header">Experience</div>
+      <div className="choices">
+        <div
+          className="work"
+          onClick={() => {
+            showProjects('work');
+          }}
+        >
+          Work
+        </div>
+        <div
+          className="projects"
+          onClick={() => {
+            showProjects('projects');
+          }}
+        >
+          Projects
+        </div>
+      </div>
+      <div className="work-container" id="work-container">
+        im work
+      </div>
+      <div className="projects-container" id="projects-container">
+        im projects
+      </div>
     </div>
   );
 };
