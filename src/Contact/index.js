39f47ba6c7f65322cbe8f67e9f29275133fdcfff
Isaiah Harris <harrisfambam@gmail.com>
2019-01-08 15:49:09 -1000

    sidebar

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -60,9 +60,9 @@ class Contact extends Component {
       textField.rows = '5';
       console.log(textField.rows);
     } else if (screenHeight > 700 && screenHeight <= 850) {
-      textField.rows = '7';
+      textField.rows = '6';
     } else if (screenHeight > 850 && screenHeight <= 1500) {
-      textField.rows = '8';
+      textField.rows = '7';
     } else if (screenHeight > 1700) {
       textField.rows = '8';
     } else if (screenHeight < 500) {
