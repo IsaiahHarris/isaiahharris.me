6f45b0e4b3815a3c2b30177a6d4e6ec37952074a
Isaiah Harris <harrisfambam@gmail.com>
2019-02-11 22:07:53 -1000

    remove comments on contact.js

--- contact.js
+++ contact.js
@@ -114,14 +114,4 @@ module.exports.sendInfo = (event, context, callback) => {
       });
     }
   });
-
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
-
-  // callback(null, response);
 };
